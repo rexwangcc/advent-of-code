1e80f9a1f8a9d82704c8f7d44fdc8359832abf20
Rex <rexwangcc@gmail.com>
2020-12-08 20:30:09 -0500

    Day8.

---
+++ 2020/Clojure/day_08/solution1.clj
@@ -0,0 +1,43 @@
+(ns solution1
+  (:require [clojure.string :as str]))
+
+
+(defn get-lines
+  "Read in and parse inputs as seq of vectors."
+  [file]
+  (letfn [(destruct
+            [line]
+            (let [[operation argument] (str/split line #" ")]
+              [operation (read-string argument)]))]
+    (->> file
+         slurp
+         str/split-lines
+         (map destruct))))
+
+(defn boot
+  "Run all instructions until repetition. Use `mapv` since we need to index the
+   index so cannot use lazy-sequence."
+  [inputs]
+  (letfn [(dispatch [[operation argument]]
+                    (case operation
+                      "jmp" [argument 0]
+                      "nop" [1 0]
+                      "acc" [1 argument]))
+          (标注 [m] (zipmap (keys m) (repeat true)))]
+    (let [inputs->map (mapv (fn [instruction] {instruction false}) inputs)]
+      (loop [inputs->map inputs->map
+             idx 0
+             accumulator 0]
+        (if (first (vals (get inputs->map idx)))
+          ;; second round detected
+          accumulator
+          ;; still the first round
+          (let [k (first (keys (get inputs->map idx)))
+                [Δidx Δaccu] (dispatch k)]
+            (recur (update-in inputs->map [idx] 标注)
+                   (+ idx Δidx)
+                   (+ accumulator Δaccu))))))))
+
+(comment
+  (boot (get-lines "./day_08/inputs.txt"))
+)
---
+++ 2020/Clojure/day_08/solution2.clj
@@ -0,0 +1,63 @@
+(ns solution2
+  (:require [clojure.string :as str]))
+
+(defn get-lines
+  "Read in and parse inputs as seq of vectors."
+  [file]
+  (letfn [(destruct
+            [line]
+            (let [[operation argument] (str/split line #" ")]
+              [operation (read-string argument)]))]
+    (->> file
+         slurp
+         str/split-lines
+         (map destruct))))
+
+(defn boot
+  "Run all instructions towards the end or repitition."
+  [inputs]
+  (letfn [(dispatch [[operation argument]]
+                    (case operation
+                      "jmp" [argument 0]
+                      "nop" [1 0]
+                      "acc" [1 argument]))
+          (标注 [m] (zipmap (keys m) (repeat true)))]
+    (let [inputs->map (mapv (fn [instruction] {instruction false}) inputs)]
+      (loop [inputs->map inputs->map
+             idx 0
+             accumulator 0]
+        (cond
+          ;; terminates, returns the result
+          (>= idx (count inputs->map)) accumulator
+          ;; second round detected, bad!
+          (first (vals (get inputs->map idx))) false
+          ;; keep running
+          :else (let [k (first (keys (get inputs->map idx)))
+                      [Δidx Δaccu] (dispatch k)]
+                  (recur (update-in inputs->map [idx] 标注)
+                         (+ idx Δidx)
+                         (+ accumulator Δaccu))))))))
+
+(defn detect-and-fix
+  "Brute-forcely detect all of jmp and nop operations
+   by outsourcing executions to boot.
+   Gotchas:
+   - should use `nth` instead of `get/get-in` on seq
+   - `update-in` expects vec not seq"
+  [inputs]
+  (letfn [(修复 [[ops arg]]
+            (case ops
+              "acc" ["acc" arg]
+              "nop" ["jmp" arg]
+              "jmp" ["nop" arg]))
+          (可修复? [ops] (#{"nop" "jmp"} ops))]
+    (for [[ops _] inputs
+          idx (range (count inputs))
+          :when (可修复? ops)
+          :let [fixed (update-in (vec inputs) [idx] 修复)
+                result (boot fixed)]
+          #_#_:while (false? result)]
+      result)))
+
+(comment
+  (first (filter int? (detect-and-fix (get-lines "./day_08/inputs.txt")))))
